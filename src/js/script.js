7d342e8161d2acfb1df2924f578f1ec09de283f6
Vladislav <vlad.tiw27@gmail.com>
2020-12-02 13:40:58 +0200

    finished promo section

--- src/js/script.js
+++ src/js/script.js
@@ -1,8 +1,9 @@
 $(document).ready(function(){
     $('.carousel').slick({
-        arrows: false,
+        arrows: true,
+        prevArrow: '<button type="button" class="slick-prev"><img src="../img/prev-arrow.png"></button>',
+        nextArrow: '<button type="button" class="slick-next"><img src="../img/next-arrow.png"></button>',
         dots: true,
-        speed: 500,
-        cssEase: 'linear'
+        speed: 400
       });
   });
