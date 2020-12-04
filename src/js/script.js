d2609268395fa57882c377feaefa64af84e50958
Vladislav <vlad.tiw27@gmail.com>
2020-12-04 20:17:25 +0200

    create first carousel with arrows and dots on js

--- src/js/script.js
+++ src/js/script.js
@@ -4,42 +4,52 @@ const carouselWrapper = document.querySelector('.promo__carousel'),
       inner = document.querySelector('.promo__carousel-inner'),
       slides = document.querySelectorAll('.promo__offer'),
       arrowNext = document.querySelector('.promo__arrow-next'),
-      arrowPrev = document.querySelector('.promo__arrow-prev');
+      arrowPrev = document.querySelector('.promo__arrow-prev'),
+      dots = document.querySelectorAll('.promo__dot');
 
-carouselWrapper.style.overflow = 'hidden';
-carouselWrapper.style.width = '100%';
-inner.style.display = 'flex';
 inner.style.width = `${100 * slides.length}%`;
 
 let slideWidth = 100 / slides.length;
-
 let availableWidth = `${slideWidth * (slides.length - 1)}`;
-console.log(availableWidth);
 
 slides.forEach((item) => {
     item.style.width = `${slideWidth}%`;
 });
 
-let count = 0;
-
-function nextSlide() {
-    if (count < +availableWidth) {
-        count += +slideWidth;
-        inner.style.transform = `translateX(-${count}%)`;
-    }
-}
-
-function prevSlide () {
-    if (count >= +availableWidth) {
-        count -= slideWidth;
-        inner.style.transform = `translateX(-${count}%)`;
-    }
-}
+let pos = 0;
 
 arrowNext.addEventListener('click', () => {
-    nextSlide();
+    pos += 1;
+    if (pos > slides.length - 1) {
+        pos = 0;
+    }
+    moveToSlide(pos);
 })
 
 arrowPrev.addEventListener('click', () => {
-    prevSlide();
+    pos -= 1;
+    if (pos < 0) {
+        pos = slides.length - 1;
+    }
+    moveToSlide(pos);
 })
+
+dots.forEach((item, i) => {
+    item.addEventListener('click', () => {
+    moveToSlide(i);
+    })
+})
+
+
+function moveToSlide(index = 1) {
+    let coord = index * slideWidth;
+    
+    dots.forEach((dot, i) => {
+        dot.classList.remove('promo__dot_active');
+        if (i === index) {
+            dot.classList.add('promo__dot_active');
+        }
+    })
+    
+    inner.style.transform = `translateX(-${coord}%)`;
+}
